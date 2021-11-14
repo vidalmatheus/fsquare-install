8dd41fd924a389c7a57d2592fc9b0170676e771b
Matheus Vidal <47875248+vidalmatheus@users.noreply.github.com>
2021-11-14 16:24:23 -0300

    Update fsquare.sh

--- fsquare.sh
+++ fsquare.sh
@@ -76,11 +76,13 @@ case $installnvm in [nN][oO]|[nN]);;*)
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
   nvm --version
+  
+  # Install & use node 16
+  nvm install 16
 esac
 
 case $installvue in [nN][oO]|[nN]);;*)
   echo -e "${GREEN}\n\t 3. Installing vue, vue/cli and vue/cli-init${RESTORE}\n"
-  nvm install 16
   npm install vue
   npm install -g @vue/cli
   npm install -g @vue/cli-init
